73b16397e15a7a8596406ad0eebc09938a59b2db
Roma Zuravliov <janmorozov1988@gmail.com>
2024-09-10 11:24:37 +0300

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,38 @@
 /* eslint-disable max-len */
 
+import { useState } from 'react';
 import './App.scss';
 import moviesFromServer from './api/movies.json';
 import { MovieList } from './components/MovieList/MovieList';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <MovieList movies={moviesFromServer} />
+export const App = () => {
+  const [query, setQuery] = useState('');
+
+  const handleInputChange = event => {
+    setQuery(event.target.value.trim());
+  };
+
+  const visibleMovies = moviesFromServer.filter(
+    movie =>
+      movie.title.toLocaleLowerCase().includes(query.toLocaleLowerCase()) ||
+      movie.description.toLocaleLowerCase().includes(query.toLocaleLowerCase()),
+  );
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <h1>Search movie</h1>
+        <input
+          type="text"
+          placeholder="Type search word"
+          value={query}
+          onChange={handleInputChange}
+        />
+        <MovieList movies={visibleMovies} />
+      </div>
+      <div className="sidebar" data-cy="Sidebar">
+        Sidebar will be here
+      </div>
     </div>
-    <div className="sidebar" data-cy="Sidebar">
-      Sidebar will be here
-    </div>
-  </div>
-);
+  );
+};
